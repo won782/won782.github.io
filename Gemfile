7dc31de1166b0da30c781bfd715944657951186f
Won Suh <won782@gmail.com>
2018-01-29 18:48:31 -0500

    qf

--- Gemfile
+++ Gemfile
@@ -20,7 +20,10 @@ gem "github-pages", group: :jekyll_plugins
 # If you have any plugins, put them here!
 group :jekyll_plugins do
   gem "jekyll-crosspost-to-medium"
+  gem "jekyll-redirect-from"
 end
+
+
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "Starting a Personal Blog",
+        "excerpt":"So here it begins!After long slackness and hiatus of 2 years or so, I am restarting my personal blog! This can serve as good learning experience as well as motiviating myself to learn more and consolidate my learnings.ContentsI will mostly talk about data science, travel, sports via data, and stuffs...","categories": ["numbers","extracurricular"],
+        "tags": ["blog","chromebook","personal blog","social media"],
+        "url": "http://0.0.0.0:4000/extracurriculars/starting_blog",
+        "teaser":null},]
